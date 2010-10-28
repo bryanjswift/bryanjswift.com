4172fc9a8531773a49135a9f443717b108405703
Bryan J Swift <bryan.j.swift@gmail.com>
2010-10-28 16:01:01 -0400

    Setting up examples of various page render methods

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,14 +2,23 @@ package com.bryanjswift
 
 import com.codahale.fig.Configuration
 import org.mortbay.jetty.Server
+import org.mortbay.jetty.webapp.WebAppContext
 
 object PortfolioApp {
   val Config = new Configuration("src/main/resources/config.json")
   def main(args:Array[String]) {
     val server = new Server(Config("http.port").as[Int]);
-    //XmlConfiguration configuration = new XmlConfiguration(new File("myJetty.xml").toURL());
-    // or use new XmlConfiguration(new FileInputStream("myJetty.xml"));
-    //configuration.configure(server);
-    server.start();
+    // create the context for the webapp
+    val webapp = Config("webapp.path").or("src/main/webapp")
+    val context = new WebAppContext()
+    context.setDescriptor(webapp + "/WEB-INF/web.xml")
+    context.setResourceBase(webapp)
+    context.setContextPath(Config("webapp.context").or("/"))
+    context.setParentLoaderPriority(true)
+    // set the webapp context as the handler
+    server.setHandler(context)
+    // start the server
+    server.start()
+    server.join()
   }
 }
---
+++ src/main/scala/com/bryanjswift/web/resources/Index.scala
@@ -0,0 +1,16 @@
+package com.bryanjswift.web.resources
+
+import com.sun.jersey.spi.resource.Singleton
+import javax.ws.rs.{GET,Produces,Path,PathParam}
+import javax.ws.rs.core.MediaType.{APPLICATION_XML,TEXT_PLAIN,TEXT_XML,TEXT_HTML}
+
+@Singleton
+@Path("/index")
+class IndexResource {
+  @GET
+  @Produces(Array(TEXT_HTML))
+  def message:String = {
+    val xml = <h1>Hello Bryan, From Jersey</h1>
+    xml.toString
+  }
+}
