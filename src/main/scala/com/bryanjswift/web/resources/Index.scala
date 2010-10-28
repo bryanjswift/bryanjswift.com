572b60137f197c0e3da44110bf6275052f9b12d6
Bryan J Swift <bryan.j.swift@gmail.com>
2010-10-28 16:17:44 -0400

    Create an example of using velocity

--- src/main/scala/com/bryanjswift/web/resources/Index.scala
+++ src/main/scala/com/bryanjswift/web/resources/Index.scala
@@ -3,6 +3,7 @@ package com.bryanjswift.web.resources
 import com.sun.jersey.spi.resource.Singleton
 import javax.ws.rs.{GET,Produces,Path,PathParam}
 import javax.ws.rs.core.MediaType.{APPLICATION_XML,TEXT_PLAIN,TEXT_XML,TEXT_HTML}
+import velocity.VelocityView
 
 @Singleton
 @Path("/index")
@@ -12,5 +13,13 @@ class IndexResource {
   def message:String = {
     val xml = <h1>Hello Bryan, From Jersey</h1>
     xml.toString
+  }
+  @GET
+  @Path("/velocity")
+  @Produces(Array(TEXT_HTML))
+  def velocity:String = {
+    val context = Map[String,Any]()
+    val view = new VelocityView("templates/index.vm")
+    view.merge(context)
   }
 }
