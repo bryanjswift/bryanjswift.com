ab41f27b7cdc1f3f30fc185201b1e59e36b52b3f
Bryan J Swift <bryan.j.swift@gmail.com>
2010-11-02 02:09:59 -0400

    Make some methods that should be private, private

--- src/main/scala/PortfolioApp.scala
+++ src/main/scala/PortfolioApp.scala
@@ -6,8 +6,8 @@ import org.mortbay.jetty.webapp.WebAppContext
 
 object PortfolioApp {
   private val ClassLoader = this.getClass.getClassLoader()
-  def stream(path: String) = ClassLoader.getResourceAsStream(path)
-  def absolutePath(filename: String) = ClassLoader.getResource(filename).getFile
+  private def stream(path: String) = ClassLoader.getResourceAsStream(path)
+  private def absolutePath(filename: String) = ClassLoader.getResource(filename).getFile
   val Config = new Configuration(stream("config.json"))
   def main(args: Array[String]) {
     val server = new Server(Config("http.port").as[Int]);
